ab5b39a98ae9880dbd6ffccad9e3c3cd9bc89fc7
Prraneth Reddy <94442954+prraneth265@users.noreply.github.com>
2022-11-14 19:22:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App'
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />ijoefwijfeiowhfo
+      <App />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
