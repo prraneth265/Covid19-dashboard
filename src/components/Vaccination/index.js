cd59fa79887c1e11f792dff570aa8f0b13693e39
Prraneth Reddy <94442954+prraneth265@users.noreply.github.com>
2025-02-28 18:24:35 +0530

    Update index.js

--- src/components/Vaccination/index.js
+++ src/components/Vaccination/index.js
@@ -8,7 +8,7 @@ import './index.css'
 class Vaccination extends Component {
   renderLoader = () => (
     <>
-      <div className="loader-container" testid="aboutRouteLoader">
+      <div className="loader-container" data-testid="aboutRouteLoader">
         <Loader type="ThreeDots" color="#ffffff" height="50" width="50" />
       </div>
     </>
