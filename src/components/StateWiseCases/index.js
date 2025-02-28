e8914334f7113aad3e62984bff0ac9c739da975c
Prraneth Reddy <94442954+prraneth265@users.noreply.github.com>
2025-02-28 18:24:12 +0530

    Update index.js

--- src/components/StateWiseCases/index.js
+++ src/components/StateWiseCases/index.js
@@ -214,7 +214,7 @@ class StateWiseCases extends Component {
 
   renderLoader = () => (
     <>
-      <div className="loader-container" testid="stateDetailsLoader">
+      <div className="loader-container" data-testid="stateDetailsLoader">
         <Loader type="ThreeDots" color="blue" height="50" width="50" />
       </div>
     </>
@@ -299,7 +299,7 @@ class StateWiseCases extends Component {
             <div className="district-data-ul-list">
               <ul
                 className="districts-container"
-                testid="topDistrictsUnorderedList"
+                data-testid="topDistrictsUnorderedList"
               >
                 {catdata.map(each => (
                   <ShowEachDistrictData
@@ -312,7 +312,7 @@ class StateWiseCases extends Component {
             </div>
           </div>
 
-          <div className="graphs-data" testid="lineChartsContainer">
+          <div className="graphs-data" data-testid="lineChartsContainer">
             <ChartsData stateCode={stateCode} category={category} />
           </div>
         </div>
